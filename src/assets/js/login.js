7e98f9df67b6ad85e74705585559bb7f792208a4
Ciollo <giorgiofasullo68@gmail.com>
2024-04-17 21:31:16 +0200

    animation + added button go to index from login

--- src/assets/js/login.js
+++ src/assets/js/login.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     setTimeout(function() {
         $("body").animate({opacity: 1}, 1000);
-    }, 1000);
+    }, 750);
 });
 
 var toggleViewPasswordElement = document.getElementById("toggleViewPassword");
@@ -9,10 +9,11 @@ var userPassword =  document.getElementById("userPassword");
 var id =  document.getElementById("cas");
 
 toggleViewPasswordElement.addEventListener("click", toggleViewPassword);
-id.addEventListener("click", function nada () {
-    var containerpass = document.getElementById("container-pass")
-    containerpass.hidden = false;
-})
+
+// id.addEventListener("click", function nada () {
+//     var containerpass = document.getElementById("container-pass")
+//     containerpass.hidden = false;
+// })
 
 function toggleViewPassword() {
     if (toggleViewPasswordElement.classList.contains("bxs-hide")) {
@@ -24,4 +25,17 @@ function toggleViewPassword() {
         toggleViewPasswordElement.classList.add("bxs-hide");
         userPassword.type = "password";
     }
+}
+
+
+var nexusLogo = document.getElementById("nexusLogo");
+nexusLogo.addEventListener("click", goToIndex);
+
+function goToIndex() {
+$(document).ready(function () {
+    $("body").fadeOut("slow", function () {
+      window.location.href = "index.html";
+    });
+});
+
 }
