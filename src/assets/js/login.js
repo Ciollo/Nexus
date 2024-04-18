01d33c3350d98459215c60188665ed55c8d4cba7
Ciollo <giorgiofasullo68@gmail.com>
2024-04-18 20:15:35 +0200

    fixed a little the login and animations

--- src/assets/js/login.js
+++ src/assets/js/login.js
@@ -1,36 +1,35 @@
-$(document).ready(function(){
-    setTimeout(function() {
-        $("body").animate({opacity: 1}, 1000);
-    }, 750);
+$(document).ready(function () {
+  setTimeout(function () {
+    $("body").animate({ opacity: 1 }, 1000);
+  }, 750);
 });
 
 var toggleViewPasswordElement = document.getElementById("toggleViewPassword");
-var userPassword =  document.getElementById("userPassword");
-var btnContinueSignIn =  document.getElementById("btn-continue");
+var userPassword = document.getElementById("userPassword");
+var btnContinueSignIn = document.getElementById("btn-continue");
 
 toggleViewPasswordElement.addEventListener("click", toggleViewPassword);
 
 btnContinueSignIn.addEventListener("click", continueSignIn);
 
 function continueSignIn() {
-    var containerpass = document.getElementById("container-pass")
-    containerpass.hidden = false;
-    btnContinueSignIn.classList.remove("btn-continue");
-    btnContinueSignIn.classList.add("btn-sign-in");
-    btnContinueSignIn.innerHTML = "Sign In";
-
+  var containerpass = document.getElementById("container-pass");
+  containerpass.hidden = false;
+  btnContinueSignIn.classList.remove("btn-continue");
+  btnContinueSignIn.classList.add("btn-sign-in");
+  btnContinueSignIn.innerHTML = "Sign In";
 }
 
 function toggleViewPassword() {
-    if (toggleViewPasswordElement.classList.contains("bxs-hide")) {
-        toggleViewPasswordElement.classList.remove("bxs-hide");
-        toggleViewPasswordElement.classList.add("bxs-show");
-        userPassword.type = "text";
-    } else {
-        toggleViewPasswordElement.classList.remove("bxs-show");
-        toggleViewPasswordElement.classList.add("bxs-hide");
-        userPassword.type = "password";
-    }
+  if (toggleViewPasswordElement.classList.contains("bxs-hide")) {
+    toggleViewPasswordElement.classList.remove("bxs-hide");
+    toggleViewPasswordElement.classList.add("bxs-show");
+    userPassword.type = "text";
+  } else {
+    toggleViewPasswordElement.classList.remove("bxs-show");
+    toggleViewPasswordElement.classList.add("bxs-hide");
+    userPassword.type = "password";
+  }
 }
 
 var linkSignUp = document.getElementById("link-sign-up");
@@ -38,22 +37,20 @@ var linkSignUp = document.getElementById("link-sign-up");
 linkSignUp.addEventListener("click", goToSignUp);
 
 function goToSignUp() {
-$(document).ready(function () {
+  $(document).ready(function () {
     $("body").fadeOut("slow", function () {
       window.location.href = "register.html";
     });
-});
-
+  });
 }
 
 var nexusLogo = document.getElementById("nexusLogo");
 nexusLogo.addEventListener("click", goToIndex);
 
 function goToIndex() {
-$(document).ready(function () {
+  $(document).ready(function () {
     $("body").fadeOut("slow", function () {
       window.location.href = "index.html";
     });
-});
-
+  });
 }
