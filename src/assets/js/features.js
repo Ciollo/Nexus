df3964373c3fdaeca8096894daa47aa51670a4a6
Ciollo <giorgiofasullo68@gmail.com>
2024-04-19 22:11:54 +0200

    navbar TODO FIX NAVBAR + BOOTSTRAP NOT WORKING

---
+++ src/assets/js/features.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+  setTimeout(function () {
+    $("body").animate({ opacity: 1 }, 1000);
+  }, 750);
+});
--- src/assets/js/home.js
+++ src/assets/js/home.js
@@ -3,3 +3,25 @@ $(document).ready(function () {
     $("body").animate({ opacity: 1 }, 1000);
   }, 750);
 });
+
+var goToSignBtn = document.getElementById("goToSignBtn");
+var goToSignUpBtn = document.getElementById("goToSignUpBtn");
+
+goToSignBtn.addEventListener("click", goToLogin);
+goToSignUpBtn.addEventListener("click", goToRegister);
+
+function goToLogin() {
+  $(document).ready(function () {
+    $("body").fadeOut("slow", function () {
+      window.location.href = "login.html";
+    });
+  });
+}
+
+function goToRegister() {
+  $(document).ready(function () {
+    $("body").fadeOut("slow", function () {
+      window.location.href = "register.html";
+    });
+  });
+}
