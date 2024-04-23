4e6141a1b0f9d6346366cc8f5dea22041b6e3820
Ciollo <93654785+Ciollo@users.noreply.github.com>
2024-04-23 09:55:22 +0200

    home

---
+++ src/assets/js/cursor.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+  var cursor = document.querySelector(".cursor-light");
+  var links = document.querySelectorAll("a");
+  var initCursor = false;
+
+  for (var i = 0; i < links.length; i++) {
+    var selfLink = links[i];
+
+    selfLink.addEventListener("mouseover", function() {
+      cursor.classList.add("cursor-light--link");
+    });
+    selfLink.addEventListener("mouseout", function() {
+      cursor.classList.remove("cursor-light--link");
+    });
+  }
+
+  window.onmousemove = function(e) {
+    var mouseX = e.clientX;
+    var mouseY = e.clientY;
+
+    if (!initCursor) {
+      // cursor.style.opacity = 1;
+      TweenLite.to(cursor, 0.3, {
+        opacity: 1
+      });
+      initCursor = true;
+    }
+
+    TweenLite.to(cursor, 0, {
+      top: mouseY + "px",
+      left: mouseX + "px"
+    });
+  };
+
+  window.onmouseout = function(e) {
+    TweenLite.to(cursor, 0.3, {
+      opacity: 0
+    });
+    initCursor = false;
+  };
+});
