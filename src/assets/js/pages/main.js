662390411741e9833e690ed4fb1e441cf6a1cc79
Ciollo <giorgiofasullo68@gmail.com>
2024-04-26 20:22:40 +0200

    basic side navbar
    TODO RESPONSIVE

--- src/assets/js/pages/main.js
+++ src/assets/js/pages/main.js
@@ -10,4 +10,14 @@ function toggleNavbar() {
     navbar.classList.toggle('navbar-side-not-active');
     navbar.classList.toggle('navbar-side-active');
 
+    let imgTitle = document.getElementById('img-title');
+    imgTitle.classList.toggle('display-none');
+    imgTitle.classList.toggle('display-block');
+
+    let textNavbar = document.querySelectorAll('.nav-link-title');
+    for (let i = 0; i < textNavbar.length; i++) {
+        let text = textNavbar[i];
+        text.classList.toggle('display-none');
+        text.classList.toggle('display-block');
+    }
 };
