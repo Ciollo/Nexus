e87bb70f5ef6d98a6677af515599b2e342eaf311
Ciollo <giorgiofasullo68@gmail.com>
2024-04-27 17:08:40 +0200

    addedd setting icon/container

--- src/assets/js/pages/main.js
+++ src/assets/js/pages/main.js
@@ -1,27 +1,27 @@
-let btnCloseOpenNavbar = document.getElementById('btn-close-open-navbar');
+let btnCloseOpenNavbar = document.getElementById("btn-close-open-navbar");
 
-btnCloseOpenNavbar.addEventListener('click', toggleNavbar);
+btnCloseOpenNavbar.addEventListener("click", toggleNavbar);
 
 function toggleNavbar() {
-    btnCloseOpenNavbar.classList.toggle('btn-navbar-not-active');
-    btnCloseOpenNavbar.classList.toggle('btn-navbar-active');
+  btnCloseOpenNavbar.classList.toggle("btn-navbar-not-active");
+  btnCloseOpenNavbar.classList.toggle("btn-navbar-active");
 
-    let navbar = document.getElementById('navbar-side');
-    navbar.classList.toggle('navbar-side-not-active');
-    navbar.classList.toggle('navbar-side-active');
+  let navbar = document.getElementById("navbar-side");
+  navbar.classList.toggle("navbar-side-not-active");
+  navbar.classList.toggle("navbar-side-active");
 
-    let imgTitle = document.getElementById('img-title');
-    imgTitle.classList.toggle('display-none');
-    imgTitle.classList.toggle('display-block');
+  let imgTitle = document.getElementById("img-title");
+  imgTitle.classList.toggle("display-none");
+  imgTitle.classList.toggle("display-block");
 
-    let textNavbar = document.querySelectorAll('.nav-link-title');
-    for (let i = 0; i < textNavbar.length; i++) {
-        let text = textNavbar[i];
-        text.classList.toggle('display-none');
-        text.classList.toggle('display-block');
-    }
+  let textNavbar = document.querySelectorAll(".nav-link-title");
+  for (let i = 0; i < textNavbar.length; i++) {
+    let text = textNavbar[i];
+    text.classList.toggle("display-none");
+    text.classList.toggle("display-block");
+  }
 
-    let mainPageContent = document.querySelector(".main-page-content");
-    mainPageContent.classList.toggle('main-page-content-not-active');
-    mainPageContent.classList.toggle('main-page-content-active');
-};
+  let mainPageContent = document.querySelector(".main-page-content");
+  mainPageContent.classList.toggle("main-page-content-not-active");
+  mainPageContent.classList.toggle("main-page-content-active");
+}
