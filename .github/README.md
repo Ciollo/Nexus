d317a439ba16ec1e61e1f26d547caaa9a51a51ee
Ciollo <93654785+Ciollo@users.noreply.github.com>
2024-05-02 10:50:04 +0200

    temp add page

--- .github/README.md
+++ .github/README.md
@@ -10,6 +10,7 @@
 
 ![HTML](https://img.shields.io/badge/HTML-239120?style=for-the-badge&logo=html5&logoColor=white)
 ![CSS](https://img.shields.io/badge/CSS-239120?&style=for-the-badge&logo=css3&logoColor=white)
+    \item ()
 ![JavaScript](https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black)
 ![PHP](https://img.shields.io/badge/PHP-777BB4?style=for-the-badge&logo=php&logoColor=white)
 ![BootStrap]( 	https://img.shields.io/badge/Bootstrap-563D7C?style=for-the-badge&logo=bootstrap&logoColor=white)
--- src/assets/js/components/addPage.js
+++ src/assets/js/components/addPage.js
@@ -1,27 +1,27 @@
-// export function addPage() {
-//   const link = document.createElement("a");
-// //   const content = document.querySelector("textarea").value;
-//   const content = '<html>test</html>';
-//   const file = new Blob([content], { type: "html" });
-//   link.href = URL.createObjectURL(file);
-//   link.download = "sample.html";
-//   link.click();
-//   URL.revokeObjectURL(link.href);
-// }
-
 export function addPage() {
+  const link = document.createElement("a");
+//   const content = document.querySelector("textarea").value;
   const content = '<html>test</html>';
-
-  fetch('/saveFile', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ filename: 'sample.html', content: content }),
-  })
-  .then(response => response.json())
-  .then(data => console.log(data.message))
-  .catch((error) => {
-    console.error('Error:', error);
-  });
+  const file = new Blob([content], { type: "html" });
+  link.href = URL.createObjectURL(file);
+  link.download = "sample.html";
+  link.click();
+  URL.revokeObjectURL(link.href);
 }
+
+// export function addPage() {
+//   const content = '<html>test</html>';
+
+//   fetch('/saveFile', {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify({ filename: 'sample.html', content: content }),
+//   })
+//   .then(response => response.json())
+//   .then(data => console.log(data.message))
+//   .catch((error) => {
+//     console.error('Error:', error);
+//   });
+// }
