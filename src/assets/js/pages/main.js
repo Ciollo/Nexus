49c99dc68ea3f95fa71f42ca113d49bdc64e0afe
Ciollo <93654785+Ciollo@users.noreply.github.com>
2024-05-07 08:30:27 +0200

    fix comments + btn close open navbar not existing anymore

--- src/assets/js/pages/main.js
+++ src/assets/js/pages/main.js
@@ -6,35 +6,35 @@ import {
   isAddComponentTriggered,
 } from "../utils/addComponent.js";
 
-let btnCloseOpenNavbar = document.getElementById("btn-close-open-navbar");
+// let btnCloseOpenNavbar = document.getElementById("btn-close-open-navbar");
 let btnSettings = document.getElementById("btn-page-settings");
 let mainOverlay = document.getElementById("main-overlay");
 let addPageBtn = document.getElementById("add-page-navbar-option");
 let userContent = document.getElementById("user-content");
 
-function toggleNavbar() {
-  btnCloseOpenNavbar.classList.toggle("btn-navbar-not-active");
-  btnCloseOpenNavbar.classList.toggle("btn-navbar-active");
+// function toggleNavbar() {
+//   btnCloseOpenNavbar.classList.toggle("btn-navbar-not-active");
+//   btnCloseOpenNavbar.classList.toggle("btn-navbar-active");
 
-  let navbar = document.getElementById("navbar-side");
-  navbar.classList.toggle("navbar-side-not-active");
-  navbar.classList.toggle("navbar-side-active");
+//   let navbar = document.getElementById("navbar-side");
+//   navbar.classList.toggle("navbar-side-not-active");
+//   navbar.classList.toggle("navbar-side-active");
 
-  let imgTitle = document.getElementById("img-title-side-navbar");
-  imgTitle.classList.toggle("display-none");
-  imgTitle.classList.toggle("display-block");
+//   let imgTitle = document.getElementById("img-title-side-navbar");
+//   imgTitle.classList.toggle("display-none");
+//   imgTitle.classList.toggle("display-block");
 
-  let textNavbar = document.querySelectorAll(".nav-link-title");
-  for (let i = 0; i < textNavbar.length; i++) {
-    let text = textNavbar[i];
-    text.classList.toggle("display-none");
-    text.classList.toggle("display-block");
-  }
+//   let textNavbar = document.querySelectorAll(".nav-link-title");
+//   for (let i = 0; i < textNavbar.length; i++) {
+//     let text = textNavbar[i];
+//     text.classList.toggle("display-none");
+//     text.classList.toggle("display-block");
+//   }
 
-  let mainPageContent = document.querySelector(".main-page-content");
-  mainPageContent.classList.toggle("main-page-content-not-active");
-  mainPageContent.classList.toggle("main-page-content-active");
-}
+//   let mainPageContent = document.querySelector(".main-page-content");
+//   mainPageContent.classList.toggle("main-page-content-not-active");
+//   mainPageContent.classList.toggle("main-page-content-active");
+// }
 
 function toggleOverlayVisibility() {
   mainOverlay.classList.toggle("display-none");
@@ -51,7 +51,7 @@ function openSettingsPanel() {
 addClickListenersToButtons();
 // addPage();
 
-btnCloseOpenNavbar.addEventListener("click", toggleNavbar);
+// btnCloseOpenNavbar.addEventListener("click", toggleNavbar);
 btnSettings.addEventListener("click", openSettingsPanel);
 mainOverlay.addEventListener("click", function (event) {
   let settingsPanel = document.getElementById("container-user-settings-panel");
