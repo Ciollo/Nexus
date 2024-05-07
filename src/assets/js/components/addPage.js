70e429c5c07101e9a187bb673b9fdd4a9a936a65
Ciollo <93654785+Ciollo@users.noreply.github.com>
2024-05-07 08:35:08 +0200

    remove add page

--- src/assets/js/components/addPage.js
+++ src/assets/js/components/addPage.js
@@ -1,13 +1,13 @@
-export function addPage() {
-  const link = document.createElement("a");
+// export function addPage() {
+  // const link = document.createElement("a");
 //   const content = document.querySelector("textarea").value;
-  const content = '<html>test</html>';
-  const file = new Blob([content], { type: "html" });
-  link.href = URL.createObjectURL(file);
-  link.download = "sample.html";
-  link.click();
-  URL.revokeObjectURL(link.href);
-}
+//   const content = '<html>test</html>';
+//   const file = new Blob([content], { type: "html" });
+//   link.href = URL.createObjectURL(file);
+//   link.download = "sample.html";
+//   link.click();
+//   URL.revokeObjectURL(link.href);
+// }
 
 // export function addPage() {
 //   const content = '<html>test</html>';
--- src/assets/js/pages/main.js
+++ src/assets/js/pages/main.js
@@ -12,30 +12,6 @@ let mainOverlay = document.getElementById("main-overlay");
 let addPageBtn = document.getElementById("add-page-navbar-option");
 let userContent = document.getElementById("user-content");
 
-// function toggleNavbar() {
-//   btnCloseOpenNavbar.classList.toggle("btn-navbar-not-active");
-//   btnCloseOpenNavbar.classList.toggle("btn-navbar-active");
-
-//   let navbar = document.getElementById("navbar-side");
-//   navbar.classList.toggle("navbar-side-not-active");
-//   navbar.classList.toggle("navbar-side-active");
-
-//   let imgTitle = document.getElementById("img-title-side-navbar");
-//   imgTitle.classList.toggle("display-none");
-//   imgTitle.classList.toggle("display-block");
-
-//   let textNavbar = document.querySelectorAll(".nav-link-title");
-//   for (let i = 0; i < textNavbar.length; i++) {
-//     let text = textNavbar[i];
-//     text.classList.toggle("display-none");
-//     text.classList.toggle("display-block");
-//   }
-
-//   let mainPageContent = document.querySelector(".main-page-content");
-//   mainPageContent.classList.toggle("main-page-content-not-active");
-//   mainPageContent.classList.toggle("main-page-content-active");
-// }
-
 function toggleOverlayVisibility() {
   mainOverlay.classList.toggle("display-none");
   mainOverlay.classList.toggle("display-block");
@@ -49,9 +25,7 @@ function openSettingsPanel() {
 }
 
 addClickListenersToButtons();
-// addPage();
 
-// btnCloseOpenNavbar.addEventListener("click", toggleNavbar);
 btnSettings.addEventListener("click", openSettingsPanel);
 mainOverlay.addEventListener("click", function (event) {
   let settingsPanel = document.getElementById("container-user-settings-panel");
